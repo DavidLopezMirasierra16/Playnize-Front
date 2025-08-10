769a976867c2243b9bb0fd6c981c75a4202d0973
DavidLopezMirasierra16 <dlm1652003@gmail.com>
2025-08-10 21:14:23 +0200

    Cambio url

--- src/componentes/Paginas/Inicio.tsx
+++ src/componentes/Paginas/Inicio.tsx
@@ -1,9 +1,7 @@
-import Banner_Home from '../../../public/imagenes/Banner_Home.png'
-
 export function Inicio() {
     return (
         <div className="ocultar-scroll p-2">
-            <img src={Banner_Home} alt="Banner del Home" className="" />
+            <img src="/imagenes/Banner_Home.png" alt="Banner del Home" className="" />
             <div className="bg-white rounded-md shadow-sm p-5 mt-6">
                 <p>Inicio</p>
             </div>
