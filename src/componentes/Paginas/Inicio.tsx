80ed5cab3e877e45efabed48029c6f19ff96f366
DavidLopezMirasierra16 <dlm1652003@gmail.com>
2025-08-11 21:19:29 +0200

    Fonts

--- src/componentes/Paginas/Inicio.tsx
+++ src/componentes/Paginas/Inicio.tsx
@@ -2,6 +2,10 @@ export function Inicio() {
     return (
         <div className="ocultar-scroll p-2">
             <img src="/imagenes/Banner_Home.png" alt="Banner del Home" className="" />
+            <div className="mt-10 font-glacial-bold text-lg md:text-2xl lg:text-4xl uppercase gap-2 flex flex-wrap justify-center">
+                <p className="">Explora una nueva forma de organizar</p>
+                <p className="bg-clip-text text-transparent bg-gradient-to-r from-pink-600 to-blue-600">torneos</p>
+            </div>
             <div className="bg-white rounded-md shadow-sm p-5 mt-6">
                 <p>Inicio</p>
             </div>
